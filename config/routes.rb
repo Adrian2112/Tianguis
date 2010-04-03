a3da7ac6b2d5c5986d344713c2df50455b5b1b6b
Adrian <bernardogzzf@gmail.com>
2010-04-03 00:25:50 -0600

    se agrego la opcion de login en la paguina principal de busqueda y se agrego la ruta /login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   
   map.signup '/signup', :controller => "usuarios", :action => "new"
+  map.login '/login', :controller => "acceso", :action => "login"
   map.perfil '/perfil', :controller => "usuarios", :action => "perfil", :id => 1
   
   map.resources :amigos
